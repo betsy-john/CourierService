c7413565fa747bb88da8fa5f04a8ce7084b34ed1
betsy-john <betsy.john@v2solution.com>
2018-10-09 15:58:42 +0530

    add esLint  modules

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 var express = require('express')
 var app = express()
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var index = require('./serverFiles/index.js');
 
+//adding statis  files
+app.use(express.static('public'));
 //To parse URL encoded data
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //To parse json data
 app.use(bodyParser.json())
 
-// app.use('/index', index);
-//time logger 
-//Middleware function to log request protocol
-app.use('/index', function(req, res, next){
-   console.log("A request for things received at " + Date.now());
-   next();
-})
-app.get('/index', function (req, res) {
-  res.send('Hello from Express')
-})
+app.use(cookieParser())
+app.set('view engine', 'pug');
+app.set('views','./serverFiles/views');
+
+app.get('/first_template', function(req, res){
+   res.render('first_view');
+});
  
 app.listen(8080)
