53e0cffc25b89e1d7261cbef6ae8f6e86cc8aa8b
betsy-john <betsy.john@v2solution.com>
2018-10-09 14:57:27 +0530

    add body-parser modules

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express')
 var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+var index = require('./serverFiles/index.js');
+
+// app.use('/index', index);
+//time logger 
+//Middleware function to log request protocol
+app.use('/index', function(req, res, next){
+   console.log("A request for things received at " + Date.now());
+   next();
+})
+app.get('/index', function (req, res) {
+  res.send('Hello from Express')
 })
  
 app.listen(8080)
