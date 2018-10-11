2a04773e4e689eabbfa61b79a722981ce8af7b63
betsy-john <betsy.john@v2solution.com>
2018-10-11 10:45:38 +0530

    sequelize database changes

--- serverFiles/databaseFiles/db_connection.js
+++ serverFiles/databaseFiles/db_connection.js
@@ -1,11 +1,12 @@
 console.log('Including sequelize connect to database.');
 
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('Couriers', 'postgres', 'postgres', {
+//const sequelize = new Sequelize('database', 'username', 'password'
+var sequelize = new Sequelize('d8uf8brg7j71no', 'acctyddcsxxwgq', '472a0b9f02f2b99a423e757b961e47728c81a6eabb0c1af5a182963dc873fd88', {
       dialect: "postgres", // or 'sqlite', 'postgres', 'mariadb'
-      host: '127.0.0.1', // or 5432 (for postgres)
+      host: 'ec2-54-243-147-162.compute-1.amazonaws.com', // or 5432 (for postgres)
     });
-console.log('Sequelize is connected to database.');
+console.log('Sequelize is connected to heroku  database.');
 sequelize
   .authenticate()
   .then(function(err) {
