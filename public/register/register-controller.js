0c30caea057a10d4e37a5e535da95ae3fcd55382
betsy-john <betsy.john@v2solution.com>
2018-10-25 11:51:23 +0530

    add ui and angular changes to register page

--- public/register/register-controller.js
+++ public/register/register-controller.js
@@ -1,3 +1,18 @@
-angular.module('CourierServices').controller('registerController', function () {
+angular.module('CourierServices').controller('registerController', function ($state) {
+var vm = this;
+vm.registerUser = {};
+vm.registeredUser = (registeredUserDetails) => {
+  console.log('registeruser', registeredUserDetails)
+  $state.go('login')
+}
+vm.submitForm = function() {
+
+  // check to make sure the form is completely valid
+  if (vm.userForm.$valid) {
+    alert('our form is amazing');
+  }
+
+};
+
 
 })
