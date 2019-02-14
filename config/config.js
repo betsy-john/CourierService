5975d8288c7ad46825930612ad4dfe692ac181a3
betsy-john <betsy.john@v2solution.com>
2019-02-14 14:45:16 +0530

    initial commit to add sequelizerc

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: "postgres",
+    password: null,
+    database: "Couriers",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  }
+};
